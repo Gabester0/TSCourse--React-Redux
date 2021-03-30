3e0cbf7b7b6c8fe53727c41a30ce739124e844a8
Gabester0 <gceipper@gmail.com>
2021-03-30 00:30:30 -0400

    initial project setup with Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-interface AppProps {
-    color?: string;
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-const App = (props: AppProps): JSX.Element=>{
-    const [counter, setCounter] = React.useState(0);
-
-    const onIncrement = (): void => {
-        setCounter(counter + 1);
-    };
-
-    const onDecrement = (): void => {
-        setCounter(counter - 1);
-    };
-    return (
-        <div>
-            <button onClick={onIncrement}>Increment</button>
-            <button onClick={onDecrement} >Decrement</button>
-            {counter}
-        </div>
-    )
-}
-
-// class App extends React.Component<AppProps> {
-//     state = { counter: 0};
-
-//     onIncrement = (): void => {
-//         this.setState({ counter: this.state.counter + 1});
-//     };
-
-//     onDecrement = (): void => {
-//         this.setState({ counter: this.state.counter - 1});
-//     };
-
-//     render(){
-//         return(
-//             <div>
-//                 <button onClick={this.onIncrement}>Increment</button>
-//                 <button onClick={this.onDecrement} >Decrement</button>
-//                 {this.state.counter}
-//             </div>
-//             )
-//     }
-// }
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
