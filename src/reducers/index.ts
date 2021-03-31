d3b47ff9b1afd4fd8420961fc44f1178c3d5b18b
Gabester0 <gceipper@gmail.com>
2021-03-30 20:29:42 -0400

    added todos reducer to reducers/index.ts

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from 'redux';
+import { todosReducer } from './todos';
+import { ToDo } from '../actions';
+
+export interface StoreState {
+    todos: ToDo[];
+}
 
 export const reducers = combineReducers({
-    counter: ()=> 1
+    todos: todosReducer
 });
