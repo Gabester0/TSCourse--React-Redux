4023928564f915b98f9cfcc3a575bbed76bcc2c5
Gabester0 <gceipper@gmail.com>
2021-04-02 01:08:44 -0400

    added Delete_ToDos to reducer

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -4,6 +4,8 @@ export const todosReducer = (state: ToDo[] = [], action: Action) =>{
     switch (action.type){
         case ActionTypes.FETCH_TODOS:
             return action.payload;
+        case ActionTypes.DELETE_TODO:
+            return state.filter((todo: ToDo)=> todo.id !== action.payload);
         default:
             return state;
     }
