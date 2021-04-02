8bfdf9dc0bf9e19256d70a8f2ebc5377a37948de
Gabester0 <gceipper@gmail.com>
2021-04-01 23:14:02 -0400

    setup fetchTodos and implemented

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,8 +10,24 @@ interface AppProps {
 
 
 export class _App extends React.Component<AppProps>{
+    onButtonClick = ():void =>{
+        this.props.fetchTodos();
+    }
+
+    renderList():JSX.Element[] {
+        return this.props.todos.map((todo: ToDo)=>{
+            return <div key={todo.id}>{todo.title}</div>
+        })
+    }
+
     render(){
-        return <div>Hi there!</div>;
+        console.log(this.props.todos)
+        return(
+            <div>
+                <button onClick={this.onButtonClick} >Fetch</button>
+                {this.renderList()}
+            </div>
+        );
     }
 }
 
